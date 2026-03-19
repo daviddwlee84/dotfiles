c99691282ae835cc96fbbe1f071ee9cc8535f3ea
Da-Wei Lee <daviddwlee84@gmail.com>
2026-03-19 16:08:43 +0800

    Name t as toggle in which-key

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- TODO: set t name as toggle in neovim menu
+-- Register <leader>t as "toggle" group in which-key
+require("which-key").add({
+  { "<leader>t", group = "toggle" },
+})
+
 -- 快速切換 Copilot 自動建議 (Ghost text)
 vim.keymap.set("n", "<leader>tp", function()
   require("copilot.suggestion").toggle_auto_trigger()
