0550b67712e3b37409dc24ca50b0b460955b056c
Da-Wei Lee <daviddwlee84@gmail.com>
2026-03-19 16:02:09 +0800

    Add Copilot config with toggle shortcut and auto disable in Markdown

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- TODO: set t name as toggle in neovim menu
+-- 快速切換 Copilot 自動建議 (Ghost text)
+vim.keymap.set("n", "<leader>tp", function()
+  require("copilot.suggestion").toggle_auto_trigger()
+  print("Copilot Auto Trigger Toggled")
+end, { desc = "Toggle Copilot Auto Trigger" })
