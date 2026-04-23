c4a04217cbdc6dd16db2723f93beef7c22742ea6
Da-Wei Lee <daviddwlee84@gmail.com>
2026-04-23 21:40:47 +0800

    tmux: add Layouts submenu to window right-click; rename Tiled → Tiled (grid)

--- dot_config/tmux/executable_menu-layouts.sh
+++ dot_config/tmux/executable_menu-layouts.sh
@@ -6,7 +6,7 @@ exec tmux display-menu -T " Layouts " -x R -y P \
   "Even vertical"   2 "select-layout even-vertical" \
   "Main horizontal" 3 "select-layout main-horizontal" \
   "Main vertical"   4 "select-layout main-vertical" \
-  "Tiled"           5 "select-layout tiled" \
+  "Tiled (grid)"    5 "select-layout tiled" \
   "Resize 75%"      "+" "resize-pane -x 75%" \
   "" "" "" \
   "Pane left"       h "select-pane -L" \
